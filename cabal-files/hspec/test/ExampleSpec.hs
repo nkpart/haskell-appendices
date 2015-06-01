a8f99017358b534724809ee64f2a2ba6c6c5e6ec
Nick Partridge <nkpart@gmail.com>
2015-06-01 20:39:55 +1000

    Adding io to examplespec

--- cabal-files/hspec/test/ExampleSpec.hs
+++ cabal-files/hspec/test/ExampleSpec.hs
@@ -5,4 +5,7 @@ import Test.Hspec
 import Example
 
 spec :: Spec
-spec = describe "succ'" . it "is succ" $ succ 1 `shouldBe` succ' 1
+spec = describe "succ'" do
+        _ <- runIO (putStrLn "Example IO")
+        it "is succ" $ succ 1 `shouldBe` succ' 1
+        prop "twice succ" $ \x -> (succ (succ x)) === x + 2
